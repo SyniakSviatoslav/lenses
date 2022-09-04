f405280480077b190e8b53099f9c9a12e974ed2d
Syniak Sviatoslav <sandmann.holzmann@gmail.com>
2022-09-04 14:29:38 +0300

    main section and slider added

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,19 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { Dropdown } from "react-bootstrap";
+import DropdownToggle from "react-bootstrap/esm/DropdownToggle";
+import Slider from "../components/Slider";
 // import { Dropdown, DropdownButton } from "react-bootstrap";
-import { Dropdown, DropdownDivider, DropdownItem } from "semantic-ui-react";
 import './home.css'
 
 const Home = () => {
-
-
-    const friendOptions = [
-        {
-            key: 'Jenny Hess',
-            text: 'Jenny Hess',
-            value: 'Jenny Hess',
-            image: { avatar: true, src: '/images/avatar/small/jenny.jpg' },
-        },
-    ]
 
 
     return (
@@ -22,28 +14,40 @@ const Home = () => {
             <div className="main-container p-4">
                 <div className="d-flex flex-column justify-content-center align-items-center h-100">
                     <h1 className="text-white mb-4">Як обрати контактні лінзи?</h1>
-                    <div className="d-flex justify-content-center align-items-center mt-4">
-                        <div className="dropdown">
-                            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown button
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <span className="dropdown-item">Something else here</span>
-                                <span className="dropdown-item">Something else here</span>
-                                <span className="dropdown-item">Something else here</span>
-                            </div>
-                        </div>
-                        <Dropdown
-                            placeholder='Select Friend'
-                            fluid
-                            simple
-                            options={friendOptions}
-                        />
-                        <button>Режим носіння</button>
-                        <button>Виробник лінз</button>
-                        <button>Вид лінз</button>
+                    <div className="d-flex w-100 justify-content-around align-items-center mt-4">
+                        <Dropdown className="main-btn">
+                            <DropdownToggle variant="main-btn" className="mr-2" id="dropdown-basic">
+                                Режим носіння
+                            </DropdownToggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Режим 1</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Режим 1</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Режим 1</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown className="main-btn">
+                            <DropdownToggle variant="main-btn" id="dropdown-basic">
+                                Виробник лінз
+                            </DropdownToggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Режим 1</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Режим 1</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Режим 1</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown className="main-btn">
+                            <DropdownToggle variant="main-btn" id="dropdown-basic">
+                                Вид лінз
+                            </DropdownToggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Режим 1</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Режим 1</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Режим 1</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
                     </div>
                 </div>
+                <Slider/>
             </div>
 
         </div>
